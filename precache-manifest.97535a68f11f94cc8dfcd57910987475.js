6603abd358a1ab8ce4116d8a57ded155a9b0e516
TrinTragula <danielescarinci42@gmail.com>
2020-01-12 04:08:02 +0100

    Updates

---
+++ precache-manifest.97535a68f11f94cc8dfcd57910987475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe84b9f0fd1c58c95df3a45ccfb0b80",
+    "url": "/scarinci.dev/index.html"
+  },
+  {
+    "revision": "c7d22f7147d8e7d799b2",
+    "url": "/scarinci.dev/static/css/main.193d2075.chunk.css"
+  },
+  {
+    "revision": "aabffe2d2ed47a808fea",
+    "url": "/scarinci.dev/static/js/2.12a17594.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/scarinci.dev/static/js/2.12a17594.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7d22f7147d8e7d799b2",
+    "url": "/scarinci.dev/static/js/main.ca025649.chunk.js"
+  },
+  {
+    "revision": "df835c842aacdd71512f",
+    "url": "/scarinci.dev/static/js/runtime-main.afb99f58.js"
+  }
+]);
