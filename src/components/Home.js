0ba31d7fefb8fc930570424c4eeec47089d8dcff
TrinTragula <danielescarinci42@gmail.com>
2020-08-24 13:17:02 +0200

    Latest projects added to the list

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
         { name: "Nuclear toi", year: "2018", link: "https://www.npmjs.com/package/nuclear-toi", description: "An npm package to query data from the Lund/LBNL Table of Isotopes" },
         { name: "MEAN", year: "2018", link: "https://github.com/TrinTragula/MEAN", description: "An Electron App for Nuclear Physics" },
         { name: "Api Trenitalia", year: "2018-2020", link: "https://www.npmjs.com/package/api-trenitalia", description: "An npm package to grab info from Trenitalia APIs" },
-
+        { name: "Cronache Maceratesi (unofficial)", year: "2020", link: "https://www.amazon.it/TrinTragula-Cronache-Maceratesi-unofficial/dp/B08BWQ32NY", description: "Una skill Alexa per leggere le ultime notizie presenti sul sito Cronache Maceratesi" },
     ];
 
     const renderHighlights = () => highlitedProjects.map(x => {
@@ -34,13 +34,13 @@ export default function Home() {
             <p>
                 I'm <b>Daniele Scarinci</b>, an italian software engineer with a degree in physics.
                 I love learning, I love creating, but most of all I love telling computers how to learn and create.
-                I graduated in 2018 with a master degree in Physics.
+                I graduated in 2018 with a master degree in Physics and since then I've been working a software engineer.
             </p>
             <p>
                 <mark className="day">By day</mark>, I'm a full stack developer working with .NET, Javascript, NodeJS, HTML, CSS, MongoDB and data analysis in Python.
             </p>
             <p>
-                <mark className="night">By night</mark>, I try to use machine learning to teach my laptop how to play tetris, program chess engines in Fortran, study Network Security, dive deep into Linux magic and make some people angry with open source software I have to take down before they get even angrier.
+                <mark className="night">By night</mark>, I try to use machine learning to teach my laptop how to play tetris, program chess engines in Fortran, study Network Security, dive deep into Linux magic and make Android apps to have fun with statistics.
             </p>
             <div className="divider"></div>
             <h3 className="info">
@@ -51,7 +51,19 @@ export default function Home() {
             </p>
             <dl className="left-padded">
                 <dt className="project-info">
-                    <a href="https://whatsapp-web.scarinci.dev/" alt="Whatsapp analyzer" target="_blank" rel="noopener noreferrer">Whatsapp analyzer</a>
+                    <a href="https://play.google.com/store/apps/details?id=dev.scarinci.chatanalyzer" alt="Chat Analyzer for WhatsApp" target="_blank" rel="noopener noreferrer">
+                        Chat Analyzer for WhatsApp (Android)
+                    </a>
+                </dt>
+                <dd className="project-info">
+                    An android app written in Kotlin that lets you analyze your WhatsApp chats and provides you with fun stats and cool graphs.
+                </dd>
+            </dl>
+            <dl className="left-padded">
+                <dt className="project-info">
+                    <a href="https://whatsapp-web.scarinci.dev/" alt="Whatsapp analyzer" target="_blank" rel="noopener noreferrer">
+                        Whatsapp analyzer (React)
+                    </a>
                 </dt>
                 <dd className="project-info">
                     A website to analyze your Whatsapp chats and create wonderful graphs and informations out of it. With the privacy of the user in mind.
@@ -62,7 +74,7 @@ export default function Home() {
                 Open source
             </h3>
             <p>
-                Some of my open source projects that I enjoyed the most coding (look at the <b>Code</b> section for even more stuff):
+                Some of my open source projects that I enjoyed the most coding (look at the <b>Code</b> section for a complete list):
             </p>
             <dl className="left-padded">
                 {renderHighlights()}
